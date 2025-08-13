59362f0c5d6487e54c246030715ad58aebbd2a0f
mazzuks <mazzuks@gmail.com>
2025-08-12 23:01:52 -0300

    Create routes.ts

---
+++ project/lib/routes.ts
@@ -0,0 +1,9 @@
+// project/lib/routes.ts
+export const ROUTES = {
+  HOME: '/',
+  RECEITAS: '/receitas',
+  LISTA: '/lista',
+  BUSCAR: '/buscar',
+  PROMOCOES: '/promocoes', // mantenha se existir
+  PERFIL: '/perfil',       // ou /login, se preferir
+}
